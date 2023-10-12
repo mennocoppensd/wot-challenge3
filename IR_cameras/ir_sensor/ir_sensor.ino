83d5fdc0a8cbbfd1992de5bceddca60c892e382a
bramvdoren <bramvand20@student.arteveldehs.be>
2023-10-12 11:13:08 +0200

    update  sensor file arduino

--- IR_cameras/ir_sensor/ir_sensor.ino
+++ IR_cameras/ir_sensor/ir_sensor.ino
@@ -4,16 +4,16 @@ const int RECV_PIN = 7;
 IRrecv irrecv(RECV_PIN);
 decode_results results;
 
-void setup(){
+void setup() {
   Serial.begin(9600);
   irrecv.enableIRIn();
-  // irrecv.blink13(true);
 }
 
-void loop(){
-  if (irrecv.decode(&results)){
-        // Serial.println(results.value, HEX);
-        Serial.println(results.value, );
-        irrecv.resume();
+void loop() {
+  if (irrecv.decode(&results)) {
+    if (results.value == 1) {
+      Serial.println(results.value);
+    }
+    irrecv.resume(); // Ontvang het volgende waarde
   }
 }
